a3389f6d159211f4409c6972dbdb0de11b326a8f
Jonathan T. Miles <jonathan.t.miles@gmail.com>
2019-10-23 14:13:40 -0700

    starter SwiftUI app

---
+++ ContactsMeetings/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ContactsMeetings
+//
+//  Created by Jonathan T. Miles on 10/23/19.
+//  Copyright © 2019 Jonathan T. Miles. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
