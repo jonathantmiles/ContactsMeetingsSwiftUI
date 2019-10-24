8c2bce6f34e892e5c23f3ae6f8a093986433dff4
Jonathan T. Miles <jonathan.t.miles@gmail.com>
2019-10-23 22:44:09 -0700

    DatePicker experiments (needd work)

--- ContactsMeetings/ContentView.swift
+++ ContactsMeetings/ContentView.swift
@@ -9,8 +9,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var date = Date()
+    let dateComponents = DatePickerComponents()
+    let style = DefaultDatePickerStyle()
+    
     var body: some View {
-        Text("Hello World")
+        Form {
+            Section {
+                DatePicker(selection: $date) {
+                    datePickerStyle(style)
+                }
+//                DatePicker(selection: $date, displayedComponents: DatePickerComponents(arrayLiteral: [datePickerStyle(<#T##style: DatePickerStyle##DatePickerStyle#>)])) {
+//                    ForEach(0 ..< 100) {_ in
+//                        Text("\(self.dateComponents.rawValue)")
+//                    }
+//
+//                }
+//                Picker(selection: <#T##Binding<_>#>, label: <#T##_#>, content: <#T##() -> _#>) {
+//
+//                }
+                
+            }
+        }
     }
 }
 
